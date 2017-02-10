243d7329e0fb6d7f254a76497f6d8c72c88bcee1
Rian Gallagher <rianogallagher@gmail.com>
2017-02-10 19:48:49 +0000

    Update Lab1_TranslateString.java

--- Lab1_TranslateString.java
+++ Lab1_TranslateString.java
@@ -1,3 +1,5 @@
+//Take in a string and print each character as a 7 bit binary ASCII number
+//Print each character and it's frequency
 import java.util.Scanner;
 import java.util.Hashtable;
 import java.util.Enumeration;
@@ -14,6 +16,7 @@ public class Lab1_TranslateString{
 			char current = input.charAt(i);
 			String num = Integer.toBinaryString((int)current); //Convert char to ASCII binary string
 
+			//This just formats the output so that there's four binary strings per line
 			if(i % 4 == 0)
 				System.out.println();
 
@@ -40,3 +43,19 @@ public class Lab1_TranslateString{
 		}
 	}
 }
+/*
+Example input: "the cat in the hat"
+Output: 1110100 1101000 1100101 0100000 
+	1100011 1100001 1110100 0100000 
+	1101001 1101110 0100000 1110100 
+	1101000 1100101 0100000 1101000 
+	1100001 1110100 
+	'i' appeared 1 times
+	' ' appeared 4 times
+	'h' appeared 3 times
+	'e' appeared 2 times
+	'c' appeared 1 times
+	'a' appeared 2 times
+	't' appeared 4 times
+	'n' appeared 1 times
+*/	
